e49f91f4ac2139923f9947b362c78cb745bf1015
dyuti04 <duttadyuti4@gmail.com>
2024-05-25 09:33:08 +0530

    added functions for yaml file, numpy and object save and load and write in mail_utils.py file

---
+++ SensorFaultPrediction/utils/mail_utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+import yaml
+import dill
+import os, sys
+from SensorFaultPrediction.exception import MLException
+from SensorFaultPrediction.logger import logging
+
+# for yaml file, to read yaml.safe_load(file); to write yaml file yaml.dump(file);
+# for numpy array, to save np.save(file, array); to load numpy array np.load(file);
+# for saving object - dill.dump(obj,file); for loading objects - dill.load(obj,file);
+
+def read_yaml_file(file_path:str)->dict:
+    try:
+        with open(file_path,'rb') as yaml_file:
+            yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise MLException(e,sys)
+
+def write_yaml_file(file_path:str, content:object,replace:bool=False)-> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,'w') as yaml_file:
+            yaml.dump(content,yaml_file)
+    except Exception as e:
+        raise MLException(e,sys)
+
+def save_numpy_array_data(file_path:str,array:np.array):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path, 'w') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise MLException(e,sys)
+
+def load_numpy_array_data(file_path:str)->np.array:
+    try:
+        with open(file_path,'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise MLException(e,sys)
+
+def save_object(file_path:str,obj:object)->None:
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'w') as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        raise MLException(e,sys)
+
+def load_object(file_path:str)->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception("File path : {} doest not exist".format(file_path))
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise MLException(e,sys)
